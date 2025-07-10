3bd81b9a76582295f1ffad0e35cd22edc77f8754
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-07-10 15:55:53 +0630

    user add

--- app/Http/Controllers/Api/PostApiController.php
+++ app/Http/Controllers/Api/PostApiController.php
@@ -13,11 +13,15 @@ class PostApiController extends Controller
      */
     public function index()
     {
-        $post = Post::latest()->take(5)->get();
+        $post = Post::with('user')->latest()->take(5)->get();
+        $userName = $post->pluck('user')->unique()->values();
+        $category = $post->pluck('category')->unique()->values();
         return response()->json([
             'status' => true,
             'message' => 'Get Posts Success',
-            'post' => $post
+            'post' => $post,
+            'userName' => $userName,
+            'category' => $category
         ], 200);
     }
 
@@ -90,7 +94,51 @@ class PostApiController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        
+         try {
+            $validator = validator($request->all(), [
+        'title' => 'required',
+        'content' => 'required',
+        'category_id' => 'required',
+        'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'status' => false,
+            'message' => "Validation failed",
+            'errors' => $validator->errors()
+        ], 409);
+    }
+
+    $post = Post::find($id);
+    $post->user_id = $request->user()->id;
+    $post->title = $request->title;
+    $post->content = $request->content;
+    $post->category_id = $request->category_id;
+
+    if ($request->hasFile('photo')) {
+        $photoPath = $request->file('photo')->store('post_photos', 'public');
+        $post->photo = $photoPath;
+    } else {
+        $post->photo = null;
+    }
+
+    $post->save();
+
+    return response()->json([
+        'status' => true,
+        'message' => "Post created successfully",
+        'post' => $post->with('user')
+    ], 201);
+    } catch (\Exception $e) {
+         return response()->json([
+        'status' => false,
+        'message' => "Post creation failed",
+        'error' => $e->getMessage() // This will show exact error
+    ], 500);
+    }
+
     }
 
     /**
